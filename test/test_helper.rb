84d0d7dc4958d3e6338ed8d30766f9dcb7b405af
CheezItMan <chris@adadevelopersacademy.org>
2019-09-10 20:12:55 -0700

    VCR configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,16 @@ Dotenv.load
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative "../lib/seven_wonders"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "test/cassettes" # where to save cassettes
+  config.hook_into :webmock  # use webmock
+  config.default_cassette_options = {
+    record: :new_episodes, # record new data when we don't have it
+    match_requests_on: [:method, :uri, :body] # match all of these
+  }
+
+  config.filter_sensitive_data("<API_TOKEN>") do
+    ENV["API_TOKEN"]
+  end
+end
