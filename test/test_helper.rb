5a622802d7057c42df4062502b99052c5529b975
CheezItMan <chris@adadevelopersacademy.org>
2019-09-10 20:08:30 -0700

    add require statements for test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 require "minitest/autorun"
 require "minitest/reporters"
+require "vcr"
+require "webmock/minitest"
+require "dotenv"
+
+Dotenv.load
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative "../lib/seven_wonders"
